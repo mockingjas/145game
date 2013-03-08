79d8d6c19d276280aece8b8369ccd2ce3108619e
Cha <mariciabalayan@yahoo.com>
2013-03-09 01:58:16 +0800

    Fixed edges

--- Bomberman/src/bomberman/Bomberman.java
+++ Bomberman/src/bomberman/Bomberman.java
@@ -19,30 +19,30 @@ public class Bomberman extends JFrame {
       
       board = new JPanel();
       layeredPane.add(board, JLayeredPane.DEFAULT_LAYER);
-      board.setLayout( new GridLayout(10, 10) );
+      board.setLayout( new GridLayout(11, 11) );
       board.setPreferredSize( boardSize );
       board.setBounds(0, 0, boardSize.width, boardSize.height);
       
       // board, white - traversible, gray - pillars
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 121; i++) {
         JPanel square = new JPanel( new BorderLayout() );
         board.add( square );
-
-        int row = (i / 10) % 2;
+        
+        int row = (i / 11) % 2;
         if (row == 0)
-            square.setBackground( i % 2 == 0 ? Color.gray : Color.white );
-        else
             square.setBackground( Color.white );
+        else
+            square.setBackground( i % 2 == 1 ? Color.white : Color.gray );
       }
       
       // Randomize wall positions (green)
       Random rand = new Random();
     
       int i = 0, wall;
-      while( i < 100 ){
-        int row = (i / 10) % 2;
-        if( row == 0 ){
-            if( i % 2 != 0 ){
+      while( i < 121 ){
+        int row = (i / 11) % 2;
+        if( row != 0 ){
+            if( i % 2 == 1 ){
                 wall = rand.nextInt(3);
                 if( wall == 0 ) board.getComponent(i).setBackground(Color.green);    
             }
